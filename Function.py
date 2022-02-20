8209a3a82c3a9d42a11e67867f6db458697a071b
Rawan <rawan.almobarak@gmail.com>
2022-02-20 04:59:37 +0000

    update Random Forest

--- Function.py
+++ Function.py
@@ -1,3 +1,20 @@
+#!/opt/conda/bin/python
+
+
+# This is our main file. It runs the accuracy functions from other files.
+
+
+
+#imports
+from Stellar_ML_Random_Forest import Accuracy
+
+
+
+#%%
+# random forest accuracy
+print("Random Forest Accuracy: ", Accuracy)
+
+
 #%%
 def knn_auc():
     auc = 0.9440833333333334
